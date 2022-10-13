e00e37353b94b47c318605502feba09302914afc
Danilo <danvalerio89@gmail.com>
2022-10-13 12:26:08 -0300

    feat: SideBar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,10 @@
+import { useIsOpen } from "../../providers/isOpen";
 import MenuBurguer from "../MenuBurguer";
-import { HeaderStyled, DivTitle } from "./styles";
+import { HeaderStyled, DivTitle, SideBar } from "./styles";
 
 export function Header() {
+  const { isOpen } = useIsOpen();
+
   return (
     <>
       <HeaderStyled>
@@ -9,14 +12,14 @@ export function Header() {
           <h1>Danilo</h1>
           <MenuBurguer />
         </DivTitle>
-        {/* <SideBar isOpen={isOpen}>
+        <SideBar isOpen={isOpen}>
           <ul id="menu">
             <li>About me</li>
             <li>Techs</li>
             <li>Projects</li>
             <li>Contact</li>
           </ul>
-        </SideBar> */}
+        </SideBar>
       </HeaderStyled>
     </>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,9 @@
 import styled, { css, keyframes } from "styled-components";
 
+interface IsOpenProps {
+  isOpen: boolean;
+}
+
 export const HeaderStyled = styled.header`
   color: white;
   justify-content: space-between;
@@ -14,72 +18,72 @@ export const DivTitle = styled.div`
   width: 90%;
 `;
 
-// export const appearFromRight = keyframes`
-// from{
-//     opacity:1;
-//     margin: 0px 0px 0px 0px;
-// }
-// to{
-//     opacity:1;
-//     margin: 0px 0px 0px -200px;
-// }
-// `;
+export const appearFromRight = keyframes`
+from{
+    opacity:1;
+    margin: 0px 0px 0px 0px;
+}
+to{
+    opacity:1;
+    margin: 0px 0px 0px -200px;
+}
+`;
 
-// export const appearFromLeft = keyframes`
-// from{
-//     opacity:1;
-//     margin: 0px 0px 0px -200px;
+export const appearFromLeft = keyframes`
+from{
+    opacity:1;
+    margin: 0px 0px 0px -200px;
 
-// }
-// to{
-//      opacity:1;
-//      margin: 0px 0px 0px 0px;
-// }`;
+}
+to{
+     opacity:1;
+     margin: 0px 0px 0px 0px;
+}`;
 
-// export const SideBar = styled.div`
-//   #menu {
-//     position: absolute;
-//     width: 100vw;
-//     height: 100vh;
-//     margin: ${(props) =>
-//       props.isOpen ? "0px 0px 0px -200px" : "0px 0px 0px 0px"};
-//     padding: 0 50px;
-//     transform: translate(100%, 0);
-//     transition: transform 0.5s ease-in-out;
-//     animation: ${(props) =>
-//       props.isOpen
-//         ? css`
-//             ${appearFromRight} 0.5s ease-in-out
-//           `
-//         : css`
-//             ${appearFromLeft} 0.5s ease-in-out
-//           `};
-//   }
+export const SideBar = styled.div<IsOpenProps>`
+  #menu {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    margin: ${(props) =>
+      props.isOpen ? "0px 0px 0px -200px" : "0px 0px 0px 0px"};
+    padding: 0 50px;
+    transform: translate(100%, 0);
+    transition: transform 0.5s ease-in-out;
+    animation: ${(props) =>
+      props.isOpen
+        ? css`
+            ${appearFromRight} 0.5s ease-in-out
+          `
+        : css`
+            ${appearFromLeft} 0.5s ease-in-out
+          `};
+  }
 
-//   #menu li {
-//     padding: 10px 0px;
-//     font-size: 22px;
-//     color: white;
-//     transition: 0.2s;
-//     color: white;
-//     display: flex;
-//     align-items: center;
-//     gap: 10px;
-//     transition: 0.2s;
-//     :hover {
-//       color: black;
-//     }
-//   }
+  #menu li {
+    padding: 10px 0px;
+    font-size: 22px;
+    color: white;
+    transition: 0.2s;
+    color: white;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    transition: 0.2s;
+    :hover {
+      color: black;
+    }
+  }
 
-//   li {
-//     color: white;
-//     display: flex;
-//     align-items: center;
-//     gap: 5px;
-//     transition: 300ms;
-//     :hover {
-//       color: black;
-//       cursor: pointer;
-//     }
-//   }
-// `;
+  li {
+    color: white;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    transition: 300ms;
+    :hover {
+      color: black;
+      cursor: pointer;
+    }
+  }
+`;
